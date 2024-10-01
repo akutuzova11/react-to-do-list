50e3806b5f99a12b769ab8ecf4e4220d2fb54c49
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-10-01 13:20:24 +0200

    align classes with BEM convention

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import "./style.css";
 
 const Buttons = ({ tasks, hideCompletedTasks }) => (
-  <div className="todoSection__buttonsContainer">
+  <div className="todoSectionContainer">
     {tasks.length > 0 && (
       <>
-        <button className="todoSection__buttons">
+        <button className="todoSectionContainer__buttons">
           {hideCompletedTasks ? "Pokaż" : "Ukryj"} ukończone
         </button>
-
         <button
-          className="todoSection__buttons"
+          className="todoSectionContainer__buttons"
           disabled={tasks.every(({ completed }) => completed)}
         >
           Ukończ wszystkie
