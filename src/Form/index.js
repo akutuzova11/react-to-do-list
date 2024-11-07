47c86fa12241ae0c15f105ffd35724f9f7601050
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-11-07 18:35:14 +0100

    autofocus on input field after the task is added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Template, Button, NewItem } from "./styled.js";
 
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -10,18 +11,19 @@ const Form = ({ addNewTask }) => {
     if (!trimmedContent) return;
     addNewTask(trimmedContent);
     setNewTaskContent("");
+    inputRef.current.focus();
   };
 
   return (
     <Template onSubmit={onFormSubmit}>
       <NewItem
+        ref={inputRef}
         value={newTaskContent}
         type="text"
         placeholder="Co jest do zrobienia?"
         onChange={({ target }) => setNewTaskContent(target.value)}
-        autoFocus
       />
-      <Button>Dodaj zadanie</Button>
+      <Button type="submit">Dodaj zadanie</Button>
     </Template>
   );
 };
