7ce861c3e2a4527c2419ba86419c9ef4207de9b6
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-11-15 14:24:53 +0100

    remove redundant elements from Tasks.js

--- src/Features/tasks/Tasks.js
+++ src/Features/tasks/Tasks.js
@@ -3,45 +3,20 @@ import TaskList from "./TaskList/index.js";
 import Buttons from "./Buttons/index.js";
 import Section from "../../Common/Section";
 import Header from "../../Common/Header";
-import {Container} from "../../Common/Container";
-import { useTasks } from "../../useTasks.js";
+import { Container } from "../../Common/Container";
 
 function Tasks() {
-  const {
-    tasks,
-    hideCompletedTasks,
-    toggleHideCompleted,
-    removeTask,
-    toggleTaskCompleted,
-    setAllCompleted,
-    addNewTask,
-  } = useTasks();
-
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form />}
       />
       <Section
         title="Lista zadań"
-        body={
-          <TaskList
-            tasks={tasks}
-            hideCompletedTasks={hideCompletedTasks}
-            removeTask={removeTask}
-            toggleTaskCompleted={toggleTaskCompleted}
-          />
-        }
-        extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hideCompletedTasks={hideCompletedTasks}
-            toggleHideCompleted={toggleHideCompleted}
-            setAllCompleted={setAllCompleted}
-          />
-        }
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
