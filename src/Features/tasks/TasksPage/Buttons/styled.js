607f3fefb951b0c8a668cc7542809725453e9f9c
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-05 13:04:16 +0100

    update styling for selectors

---
+++ src/Features/tasks/TasksPage/Buttons/styled.js
@@ -0,0 +1,36 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Selectors = styled.button`
+  background-color: transparent;
+  border: none;
+  color: ${({theme})=>theme.colors.primary};
+  margin-left: 10px;
+
+  @media (max-width:  ${({theme}) => theme.breakpoints.mobile}px) {
+    margin: 10px;
+    flex-basis: 100%;
+  }
+
+  &:hover {
+    color: ${({theme})=>theme.colors.buttonSubmitHover};
+  }
+
+  &:active {
+    color: ${({theme})=>theme.colors.buttonSubmitActive};
+  }
+
+  &:disabled {
+    color: ${({theme})=>theme.colors.buttonDisabled};
+  }
+
+  ${({ isHidden }) =>
+    isHidden &&
+    css`
+      visibility: hidden;
+    `}
+`;
