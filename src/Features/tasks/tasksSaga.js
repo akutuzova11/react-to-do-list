087b6c629f77e980d1680a0386afe083aced4ec6
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-05 13:10:29 +0100

    implement root saga with tasksSaga

---
+++ src/Features/tasks/tasksSaga.js
@@ -0,0 +1,34 @@
+import {
+  debounce,
+  call,
+  put,
+  delay,
+  select,
+  takeEvery,
+} from "redux-saga/effects";
+import { fetchExampleTasks, setTasks, selectTasks } from "./tasksSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+  console.log("Zaczynam pracę");
+  try {
+    const tasks = yield select(selectTasks);
+    console.log(tasks);
+    yield delay(1000);
+    const exampleTasks = yield call(getExampleTasks);
+    yield put(setTasks(exampleTasks.tasks));
+  } catch (error) {
+    yield call(alert, "Coś poszło nie tak");
+  }
+}
+
+function* saveTasksInLocalStorageHandler() {
+  const tasks = yield select(selectTasks);
+  yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+  yield debounce(2000, fetchExampleTasks.type, fetchExampleTasksHandler);
+  yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
