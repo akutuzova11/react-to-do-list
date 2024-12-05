04155a5fc618de4a45fa805bc9e0ff8a3fe4f198
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-05 13:03:47 +0100

    update styling for buttons

---
+++ src/Features/tasks/TasksPage/Form/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Template = styled.form`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap: 20px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    grid-template-columns: 1fr;
+    gap: 10px;
+  }
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.buttonText};
+  border: none;
+  transition: 0.5s;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.buttonSubmitHover};
+    transform: scale(1.1);
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.colors.buttonSubmitHover};
+  }
+`;
