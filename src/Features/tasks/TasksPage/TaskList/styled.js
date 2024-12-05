6bd602e6c3fd9b219074bfd1744aa07e3f822691
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-05 12:59:44 +0100

    update styles for tasklist

---
+++ src/Features/tasks/TasksPage/TaskList/styled.js
@@ -0,0 +1,63 @@
+import styled, { css } from "styled-components";
+
+export const List = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+`;
+
+export const Item = styled.li`
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  align-items: center;
+  border-bottom: 2px solid #ddd;
+  padding: 10px;
+  gap: 10px;
+
+  ${({ hidden }) =>
+    hidden &&
+    css`
+      display: none;
+    `}
+`;
+
+export const Content = styled.span`
+  ${({ completed }) =>
+    completed &&
+    css`
+      text-decoration: line-through;
+    `}
+`;
+
+export const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.buttonText};
+  border: none;
+  width: 30px;
+  height: 30px;
+  padding: 5px;
+  margin: 0;
+  transition: background 0.4s;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.buttonCompletedHover};
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.colors.buttonCompletedActive};
+  }
+
+  ${({ $isDeleted }) =>
+    $isDeleted &&
+    css`
+      background-color: ${({ theme }) => theme.colors.ButtonRemove};
+
+      &:hover {
+        background-color: ${({ theme }) => theme.colors.ButtonRemoveHover};
+      }
+
+      &:active {
+        background-color: ${({ theme }) => theme.colors.ButtonRemoveActive};
+      }
+    `}
+`;
