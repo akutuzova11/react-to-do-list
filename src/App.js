f4e6caa4f7dbd37d1a542ea2ef3750f762ab7037
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-05 13:06:30 +0100

    add navigation to the App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
-import Tasks from "./Features/tasks/Tasks";
+import AuthorPage from "./Features/author/AuthorPage";
+import TasksPage from "./Features/tasks/TasksPage/index";
+import TaskPage from "./Features/tasks/TaskPage/index";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { StyledNavLink } from "./styled";
 
-export default () => <Tasks />;
+export default () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/autor">O autorze</StyledNavLink>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/zadania">
+          <TasksPage />
+        </Route>
+        <Route path="/autor">
+          <AuthorPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/zadania" />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
