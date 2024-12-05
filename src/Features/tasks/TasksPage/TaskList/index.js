5b30087042fd99eeffcc8e8eeb5f4fd299f2816d
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-05 18:21:26 +0100

    implement styled component into the to do list

--- src/Features/tasks/TasksPage/TaskList/index.js
+++ src/Features/tasks/TasksPage/TaskList/index.js
@@ -1,14 +1,13 @@
-import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { List, Item, Content, Button } from "./styled.js";
+import { useLocation } from "react-router-dom";
+import { List, Item, Content, Button, LinkStyled } from "./styled.js";
 import {
   toggleTaskCompleted,
   selectHideCompleted,
   removeTask,
   selectTasksByQuery,
 } from "../../TasksSlice.js";
-import { Link, useLocation } from "react-router-dom";
-import searchQueryParamName from "../searchQueryParamName.js";
+import searchQueryParamName from "../../searchQueryParamName.js";
 
 const TaskList = () => {
   const location = useLocation();
@@ -27,7 +26,7 @@ const TaskList = () => {
             {task.completed ? "✔" : ""}
           </Button>
           <Content completed={task.completed}>
-            <Link to={`/zadania/${task.id}`}>{task.content}</Link>
+            <LinkStyled to={`/zadania/${task.id}`}>{task.content}</LinkStyled>
           </Content>
           <Button $isDeleted onClick={() => dispatch(removeTask(task.id))}>
             🗑
