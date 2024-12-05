90266b4cb74fe006720d45b620e1d28757cada15
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-05 13:23:49 +0100

    add description for tasks and error handling

---
+++ src/Features/tasks/TaskPage/index.js
@@ -0,0 +1,27 @@
+import Section from "../../../Common/Section/index.js";
+import Header from "../../../Common/Header/index.js";
+import { Container } from "../../../Common/Container/index.js";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTaskByID } from "../tasksSlice.js";
+
+function TaskPage() {
+  const { id } = useParams();
+  const task = useSelector((state) => getTaskByID(state, id));
+  return (
+    <Container>
+      <Header title="Szczegóły zadania" />
+      <Section
+        title={task ? task.content : "Nie znaleziono zadania 😿"}
+        body={
+          <>
+            <strong>Ukończono: </strong>
+            {task.completed ? "Tak" : "Nie"}
+          </>
+        }
+      />
+    </Container>
+  );
+}
+
+export default TaskPage;
