7847252b6865a2be4464fa015f9a1ee0b77bba56
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-05 13:12:52 +0100

    setup Redux store with tasksReducer and rootSaga

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 
 import tasksReducer from "./Features/tasks/tasksSlice";
+import rootSaga from "./Features/tasks/rootSaga";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
   reducer: {
     tasks: tasksReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
